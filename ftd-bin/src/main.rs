3a4be2becc6e24e89aa5d297fcbdbf618a669f2b
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-17 21:14:58 -0300

    feat: create method for send request by device

--- ftd-bin/src/main.rs
+++ ftd-bin/src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use anyhow::{Context as AnyHowContext, Result};
 use rusb::{Context, Device, DeviceHandle, UsbContext};
 
@@ -7,6 +9,15 @@ const PRODUCT_ID: u16 = 0x6811;
 const BUTTONS_INTERAFCE: u8 = 1;
 const TABLET_INTERFACE: u8 = 2;
 
+struct MessageDevice {
+    pub request_type: u8,
+    pub request: u8,
+    pub value: u16,
+    pub interface: u8,
+    pub payload: Vec<u8>,
+    pub timeout: Duration,
+}
+
 fn main() -> Result<()> {
     let mut context = Context::new()?;
 
@@ -14,6 +25,17 @@ fn main() -> Result<()> {
         open_device(&mut context, VENDOR_ID, PRODUCT_ID)?.context("Tablet Not Found")?;
 
     claim_interfaces(&mut handle, &[BUTTONS_INTERAFCE, TABLET_INTERFACE])?;
+
+    let mtm_1106_init = MessageDevice {
+        request_type: 0x21,
+        request: 0x09,
+        value: 0x0302,
+        interface: TABLET_INTERFACE as u8,
+        payload: vec![0x02, 0x02, 0xb5, 0x02, 0x00, 0x00, 0x00, 0x00],
+        timeout: Duration::from_secs(1),
+    };
+
+    send_to_device(&mut handle, &mtm_1106_init)?;
 
     Ok(())
 }
@@ -44,6 +66,22 @@ fn claim_interfaces<T: UsbContext>(handle: &mut DeviceHandle<T>, interfaces: &[u
         }
         handle.claim_interface(*num)?;
     }
+
+    Ok(())
+}
+
+fn send_to_device<T: UsbContext>(
+    handle: &mut DeviceHandle<T>,
+    message: &MessageDevice,
+) -> Result<()> {
+    handle.write_control(
+        message.request_type,
+        message.request,
+        message.value,
+        message.interface as u16,
+        &message.payload,
+        message.timeout,
+    )?;
 
     Ok(())
 }
