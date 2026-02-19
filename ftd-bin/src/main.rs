9eb4d8f7216c26def232b917d582a862139963af
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-18 23:55:15 -0300

    feat: read bytes for interface by device

--- ftd-bin/src/main.rs
+++ ftd-bin/src/main.rs
@@ -1,13 +1,44 @@
-use std::time::Duration;
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+    sync::{Arc, atomic::AtomicBool},
+    time::Duration,
+};
 
 use anyhow::{Context as AnyHowContext, Result};
-use rusb::{Context, Device, DeviceHandle, UsbContext};
+use rusb::{Context, Device, DeviceHandle, Direction, Result as RusbResult, UsbContext};
 
 const VENDOR_ID: u16 = 0x08f2;
 const PRODUCT_ID: u16 = 0x6811;
 
 const BUTTONS_INTERAFCE: u8 = 1;
 const TABLET_INTERFACE: u8 = 2;
+
+struct USBDevice<T: UsbContext> {
+    pub device: Device<T>,
+    pub handle: DeviceHandle<T>,
+    pub interfaces: HashMap<u8, InterfaceInfo>,
+}
+
+impl<T: UsbContext> Drop for USBDevice<T> {
+    fn drop(&mut self) {
+        let interfaces: Vec<u8> = self.interfaces.keys().into_iter().map(|v| *v).collect();
+
+        for i in interfaces {
+            if let Ok(res) = self.handle.kernel_driver_active(i) {
+                if res {
+                    let _ = self.handle.attach_kernel_driver(i);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Hash, Clone)]
+pub struct InterfaceInfo {
+    pub number: u8,
+    pub endpoint: u8,
+}
 
 struct MessageDevice {
     pub request_type: u8,
@@ -19,12 +50,23 @@ struct MessageDevice {
 }
 
 fn main() -> Result<()> {
+    let running = Arc::new(AtomicBool::new(true));
+
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, std::sync::atomic::Ordering::SeqCst);
+    })
+    .expect("Unlonw handle error");
+
     let mut context = Context::new()?;
 
-    let (_device, mut handle) =
+    let mut usb_device =
         open_device(&mut context, VENDOR_ID, PRODUCT_ID)?.context("Tablet Not Found")?;
 
-    claim_interfaces(&mut handle, &[BUTTONS_INTERAFCE, TABLET_INTERFACE])?;
+    claim_interfaces(
+        &mut usb_device.handle,
+        &[BUTTONS_INTERAFCE, TABLET_INTERFACE],
+    )?;
 
     let mtm_1106_init = MessageDevice {
         request_type: 0x21,
@@ -35,16 +77,46 @@ fn main() -> Result<()> {
         timeout: Duration::from_secs(1),
     };
 
-    send_to_device(&mut handle, &mtm_1106_init)?;
+    send_to_device(&mut usb_device.handle, &mtm_1106_init)?;
+
+    while running.load(std::sync::atomic::Ordering::SeqCst) {
+        match read_device(
+            &mut usb_device.handle,
+            usb_device.interfaces.get(&2).unwrap(),
+            100,
+        ) {
+            Ok((id, bytes)) => println!("Interface: {id} || Bytes: {bytes:02X?}"),
+            Err(rusb::Error::Timeout) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+            }
+            Err(e) => {
+                println!("Erro fatal na leitura: {:?}", e);
+                break;
+            }
+        }
+
+        match read_device(
+            &mut usb_device.handle,
+            usb_device.interfaces.get(&1).unwrap(),
+            10,
+        ) {
+            Ok((id, bytes)) => println!("Interface: {id} || Bytes: {bytes:02X?}"),
+            Err(rusb::Error::Timeout) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+            }
+            Err(e) => {
+                println!("Erro fatal na leitura: {:?}", e);
+                break;
+            }
+        }
+    }
 
     Ok(())
 }
 
-fn open_device<T: UsbContext>(
-    context: &mut T,
-    vid: u16,
-    pid: u16,
-) -> Result<Option<(Device<T>, DeviceHandle<T>)>> {
+fn open_device<T: UsbContext>(context: &mut T, vid: u16, pid: u16) -> Result<Option<USBDevice<T>>> {
     let devices = context.devices()?;
 
     for device in devices.iter() {
@@ -52,7 +124,27 @@ fn open_device<T: UsbContext>(
 
         if desc.vendor_id() == vid && desc.product_id() == pid {
             let handle = device.open()?;
-            return Ok(Some((device, handle)));
+            let mut interfaces = HashMap::new();
+
+            let config_descriptor = device.active_config_descriptor()?;
+            for int in config_descriptor.interfaces() {
+                let number = int.number();
+                for desc in int.descriptors() {
+                    for endpoint in desc.endpoint_descriptors() {
+                        if endpoint.direction() == Direction::In {
+                            let endpoint = endpoint.address();
+                            interfaces.insert(number, InterfaceInfo { number, endpoint });
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return Ok(Some(USBDevice {
+                device: device,
+                handle: handle,
+                interfaces,
+            }));
         }
     }
 
@@ -84,4 +176,20 @@ fn send_to_device<T: UsbContext>(
     )?;
 
     Ok(())
+}
+
+fn read_device<T: UsbContext>(
+    handle: &mut DeviceHandle<T>,
+    interface: &InterfaceInfo,
+    timeout: u64,
+) -> RusbResult<(u8, Vec<u8>)> {
+    let mut buffer = vec![0; 64];
+
+    let reads = handle.read_interrupt(
+        interface.endpoint,
+        &mut buffer,
+        Duration::from_millis(timeout),
+    )?;
+    println!("{reads}");
+    Ok((interface.number, buffer[..reads].to_vec()))
 }
